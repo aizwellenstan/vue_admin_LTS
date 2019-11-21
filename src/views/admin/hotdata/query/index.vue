2a2993bf7b1134c850d06761a629a5628f27c361
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 14:28:46 +0900

    query hotdata

--- src/views/admin/hotdata/query/index.vue
+++ src/views/admin/hotdata/query/index.vue
@@ -1,29 +1,15 @@
 <template>
   <div>
     <div class="el-card login-card text-primary fs-xl is-always-shadow">
-      <div class="el-card__header">查詢帳戶</div>
+      <div class="el-card__header">Query HotData</div>
       <div class="el-card__body">
-        <label>查詢</label>
-        <input v-model="search" type="text" placeholder="輸入關鍵字">
-        <div>
-          <table v-for="(val, key, index) in filteredList" :key="index" border="1" style="color:black;" width="242px">
-            <tr><td width="92px">使用者名稱</td><td>{{ key }} </td></tr>
-            <tr><td width="92px">Email</td><td>{{ val.Email }}</td></tr>
-            <tr><td width="92px">姓</td><td>{{ val["First Name"] }}</td></tr>
-            <tr><td width="92px">名</td><td>{{ val["Last Name"] }}</td></tr>
-            <tr><td width="92px">已啟用</td><td>{{ val["is active"] }}</td></tr>
-            <tr>
-              <td>權限</td>
-              <!-- <td v-if=(val["is staff"]+ val["is superuser"])===2>最高管理者 </td> -->
-              <!-- <td v-else-if="val["is staff"]+ val["is superuser"]==1">管理者
-              <td v-else>員工</td>   -->
-              <td>{{ permission(val) }}</td>
-
-            </tr>
-            <br>
-          </table>
-
-        </div>
+        <!-- {{this.hotdataList}} -->
+        <table border="1" style="color:black;">
+          <tr><th>ObjectId</th><th>Edit</th></tr>
+          <tr v-for="(val, key, index) in hotdataList" :key="index">
+            <td>{{ val.ObjectData.ObjectId }} </td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
@@ -31,30 +17,20 @@
 
 <script>
 import api from '../../../../../api.js'
-const QUERY_URL = api + '/account/search/'
+const CompanyId = localStorage.getItem('CompanyId')
+const ProductId = localStorage.getItem('ProductId')
+const ProjectId = localStorage.getItem('ProjectId')
+const QUERY_URL = api + `/${CompanyId}/${ProductId}/${ProjectId}/hotdata`
 export default {
   data() {
     return {
       search: '',
-      userslist: ''
-    }
-  },
-  computed: {
-    filteredList() {
-      return Object.keys(this.userslist)
-        .filter(key => (key).toLowerCase().indexOf(this.search.toLowerCase()) !== -1)
-        .reduce((obj, key) => {
-          obj[key] = this.userslist[key]
-          return obj
-        }, {})
-      // return Object.entries(this.userslist).map(([key, value]) => ({key, value}))
-      //         .filter((data) =>
-      //         JSON.stringify(data).toLowerCase().indexOf(this.search.toLowerCase()) !== -1);
+      hotdataList: []
     }
   },
   mounted() {
     fetch(QUERY_URL, {
-      method: 'post',
+      method: 'get',
       headers: {
         'Token': localStorage.getItem('token')
       }
@@ -66,37 +42,9 @@ export default {
           }))
       )
       .then(json => {
-        this.userslist = Object(json.data)
+        this.hotdataList = Object(json.data)
+        console.log(this.hotdataList)
       })
-  },
-  methods: {
-    permission: function(val) {
-      if (val['is staff'] + val['is superuser'] === 2) {
-        return '最高管理者'
-      } else if (val['is staff'] + val['is superuser'] === 1) {
-        return '管理者'
-      } else {
-        return '員工'
-      }
-    },
-    searchData() {
-      fetch(QUERY_URL, {
-        method: 'post',
-        headers: {
-          'Token': localStorage.getItem('token')
-        }
-      })
-        .then(
-          response =>
-            response.json().then(data => ({
-              data: data
-            }))
-        )
-        .then(json => {
-          this.userslist = Object(json.data)
-          console.log(json.data)
-        })
-    }
   }
 }
 </script>
