605ccb1d3b0dfb81b470ebb7cf7f3957cce5e55f
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 14:52:24 +0900

    fix chart

--- src/views/admin/report/index.vue
+++ src/views/admin/report/index.vue
@@ -7,6 +7,6 @@
 
 <style scoped>
 .chart{
- position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;
+ position:fixed; top:0; left:210; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;
 }
 </style>
