0ac5a9671efb3ce72dcf8ca582e8bbb87d70ef68
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 15:06:39 +0900

    fix hotdata

--- src/views/admin/accounts/query/index.vue
+++ src/views/admin/accounts/query/index.vue
@@ -6,7 +6,7 @@
         <label>查詢</label>
         <input v-model="search" type="text" placeholder="輸入關鍵字">
         <div>
-          <table v-for="(val, key, index) in filteredList" :key="index" border="1" style="color:black;" width="242px">
+          <!-- <table v-for="(val, key, index) in filteredList" :key="index" border="1" style="color:black;" width="242px">
             <tr><td width="92px">使用者名稱</td><td>{{ key }} </td></tr>
             <tr><td width="92px">Email</td><td>{{ val.Email }}</td></tr>
             <tr><td width="92px">姓</td><td>{{ val["First Name"] }}</td></tr>
@@ -14,14 +14,11 @@
             <tr><td width="92px">已啟用</td><td>{{ val["is active"] }}</td></tr>
             <tr>
               <td>權限</td>
-              <!-- <td v-if=(val["is staff"]+ val["is superuser"])===2>最高管理者 </td> -->
-              <!-- <td v-else-if="val["is staff"]+ val["is superuser"]==1">管理者
-              <td v-else>員工</td>   -->
               <td>{{ permission(val) }}</td>
 
             </tr>
             <br>
-          </table>
+          </table> -->
 
         </div>
       </div>
