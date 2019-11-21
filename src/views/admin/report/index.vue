8516003f86b3622a4e49577db5b6160dfa20cf16
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 14:48:23 +0900

    fix chart

--- src/views/admin/report/index.vue
+++ src/views/admin/report/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     Chart
-    <iframe src="http://220.128.216.143:3000/d/gREAuPpWz/siemens_demo_2?orgId=1&from=now-3h&to=now&kiosk&refresh=5s"></iframe>
+    <iframe class="chart" src="http://220.128.216.143:3000/d/gREAuPpWz/siemens_demo_2?orgId=1&from=now-3h&to=now&kiosk&refresh=5s"></iframe>
   </div>
 </template>
+
+<style scoped>
+.chart{
+  width: 100%
+}
+</style>
